1eb2f785354a147e8cd12ed7fdbcbce73f9becc5
rubenmoya <rmoyarodriguez@gmail.com>
2018-04-30 16:49:11 +0200

    Improve Offer types

--- server/api/graphql/index.js
+++ server/api/graphql/index.js
@@ -1,7 +1,10 @@
 import path from 'path';
+import GraphQLJSON from 'graphql-type-json';
 import { fileLoader, mergeResolvers, mergeTypes } from 'merge-graphql-schemas';
 
 export const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './types')));
 export const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers')));
+
+resolvers.JSON = GraphQLJSON;
 
 export default { typeDefs, resolvers };
