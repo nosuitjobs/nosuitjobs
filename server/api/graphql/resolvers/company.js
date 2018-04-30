bef707da5895558b39420c5d49084040cead50c2
Mikel Garcia <mikel_6@hotmail.com>
2018-04-30 21:34:58 +0200

    Add login mutation to company resolver

--- server/api/graphql/resolvers/company.js
+++ server/api/graphql/resolvers/company.js
@@ -1,8 +1,20 @@
+import jwt from 'jsonwebtoken';
 import { Company } from '../../db/models';
+
+const SECRET_KEY = "RubenEsUnMariquita";
 
 export default {
   Query: {
     company: (parent, args, context) => Company.findById(args.id),
     companies: (parent, args, context) => Company.findAll(),
-  }
+  },
+  Mutation: {
+    login: async (parent, args, context) => {
+      const company = await Company.find(args);
+      console.log(company);
+      token = jwt.sign(company.id, SECRET_KEY);
+      console.log(token);
+      return { company, token };
+    },
+  },
 };
